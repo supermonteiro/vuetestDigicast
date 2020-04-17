ecf290f787acdc8f134c51f1b1778b9a31ce47e2
Thiago Monteiro Barbosa <supermonteiro@gmail.com>
2020-04-17 17:44:03 -0400

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="app">
+        <nav>
+            <router-link to="/">Home</router-link>
+            <router-link to="/gallery">Gallery</router-link>
+        </nav>
+        <router-view/>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+Vue.use(VueAxios, axios);
+
+export default {
+    name: 'App',
+};
+</script>
+
+<style lang="scss">
+body {
+    color: #2c3e50;
+    text-align: center;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
+a {
+    margin-right: 10%;
+}
+
+nav {
+    background-image: linear-gradient(-45deg, blue, lightblue);
+    width: 50%;
+    display: inline-block;
+    text-align: center;
+}
+
+</style>
---
+++ src/views/Gallery.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="gallery">
+        <h1>Gallery</h1>
+
+        <h2>There are **** items</h2>
+
+        <div>
+            <h3>Item Concept</h3>
+            <img src="/img/item-sample.png"
+                alt="Item Concept"
+                width="962"
+                height="544">
+            <p>(delete this part)</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Gallery',
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
